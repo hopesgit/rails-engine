274880cf5946b7553f3a263b99d0840cba0dff2f
Hope Gochnour <hope.gochnour@gmail.com>
2020-12-18 03:17:27 -0700

    Create Merchants show spec

--- app/controllers/api/v1/merchants_controller.rb
+++ app/controllers/api/v1/merchants_controller.rb
@@ -1,12 +1,38 @@
 class Api::V1::MerchantsController < ApplicationController
   def index
-    limit = 20
-    limit = params[:limit] if !params[:limit].nil? && params[:limit].to_i.class == Integer
+    limit = 100
+    limit = params[:limit].to_i if !params[:limit].nil? && params[:limit].to_i.class == Integer
     render json: MerchantSerializer.new(MerchantFacade.merchants(limit))
   end
 
   def show
     id = params[:id] if params[:id].to_i.class == Integer
     render json: MerchantSerializer.new(MerchantFacade.merchant(id))
+  end
+
+  def create
+    render json: MerchantSerializer.new(MerchantFacade.new_merchant(params[:name]))
+  end
+
+  def update
+    render json: MerchantSerializer.new(MerchantFacade.update_merchant(update_params))
+  end
+
+  def destroy
+    destruction = MerchantFacade.destroy_merchant(destroy_params)
+    head :no_content if destruction
+    render json: MerchantSerializer.new(destruction) if !destruction
+  end
+
+  private
+  def update_params
+    update = {}
+    update[:name] = params.require(:merchant).permit(:name)[:name]
+    update[:id] = params.permit(:id)[:id]
+    return update
+  end
+
+  def destroy_params
+    params.permit(:id)
   end
 end
--- app/facades/merchant_facade.rb
+++ app/facades/merchant_facade.rb
@@ -1,9 +1,30 @@
 class MerchantFacade
   def self.merchants(limit)
-    Merchant.all[0..(limit.to_i - 1)]
+    Merchant.first(limit)
   end
 
   def self.merchant(id)
-    Merchant.find(id)
+    merchant = Merchant.find_by(id: id)
+    return merchant
+  end
+
+  def self.new_merchant(name)
+    Merchant.create(name: name)
+  end
+
+  def self.update_merchant(params)
+    merchant = Merchant.find(params[:id])
+    merchant.update(name: params[:name])
+    merchant.reload
+    return merchant
+  end
+
+  def self.destroy_merchant(params)
+    merchant = Merchant.find_by(id: params[:id])
+    if !merchant.nil?
+      merchant.destroy
+    else
+      return false
+    end
   end
 end
